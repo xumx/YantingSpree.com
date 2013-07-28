7a30b67d137d232694d195b9811a030e126f7e9d
Max <deepthought@gmail.com>
2013-07-29 00:03:51 +0800

    remove insecure package

--- collections/collections.js
+++ collections/collections.js
@@ -5,3 +5,51 @@ Spree = new Meteor.Collection('sprees');
 Order = new Meteor.Collection('orders');
 
 Payment = new Meteor.Collection('payments');
+
+Merchant.allow({
+  insert: function (userId, doc) {
+	return (Roles.userIsInRole(userId, ['admin']));
+  },
+  update: function (userId, doc, fields, modifier) {
+  	return (Roles.userIsInRole(userId, ['admin']));
+  },
+  remove: function (userId, doc) {
+	return (Roles.userIsInRole(userId, ['admin']));
+  }
+});
+
+Spree.allow({
+  insert: function (userId, doc) {
+	return (Roles.userIsInRole(userId, ['admin']));
+  },
+  update: function (userId, doc, fields, modifier) {
+  	return (Roles.userIsInRole(userId, ['admin']));
+  },
+  remove: function (userId, doc) {
+	return (Roles.userIsInRole(userId, ['admin']));
+  }
+});
+
+Order.allow({
+  insert: function (userId, doc) {
+	return (userId && doc.user === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+  	return (userId && (doc.user === userId || Roles.userIsInRole(userId, ['admin'])));
+  },
+  remove: function (userId, doc) {
+	return (userId && (doc.user === userId || Roles.userIsInRole(userId, ['admin'])));
+  }
+});
+
+Payment.allow({
+  insert: function (userId, doc) {
+	return (userId && doc.user === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+  	return (userId && (doc.user === userId || Roles.userIsInRole(userId, ['admin'])));
+  },
+  remove: function (userId, doc) {
+	return (userId && (doc.user === userId || Roles.userIsInRole(userId, ['admin'])));
+  }
+});
